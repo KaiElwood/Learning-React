0a09878185c8243b376fd3e3d2f69ec15ef2f157
KED <kaipie3@gmail.com>
2021-02-15 10:00:34 -0500

    broken homepage into smaller components, finished styling menu item

--- ecommerce-project/crwn-clothing/src/components/menu-item/menu-item.component.jsx
+++ ecommerce-project/crwn-clothing/src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const MenuItem = ({ title }) => (
-    <div className='menu-item'>
+import './menu-item.scss'
+
+const MenuItem = ({ title, imageUrl, size }) => (
+    <div className={`${size} menu-item`}>
+        <div style={{
+        backgroundImage: `url(${imageUrl})`
+        }} className='background-image'> 
+
+        </div>
         <div className='content'>
-            <h1 className='title'>{title}</h1>
+            <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
