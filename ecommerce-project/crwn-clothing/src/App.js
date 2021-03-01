3ebcec33ee9affe2dc5723c09c0cd3d9180f8ac6
KED <kaipie3@gmail.com>
2021-03-01 16:42:56 -0500

    completed firebase integration and updated app

--- ecommerce-project/crwn-clothing/src/App.js
+++ ecommerce-project/crwn-clothing/src/App.js
@@ -7,12 +7,38 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
+class App extends React.Component{
+  
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    // firebase sends out a message whenever a user signs in or out, but it's an open connection all the time – we need to close it so that it logs users out essentially. we can do this with unsubscribe
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user})
+
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
     <div>
       {/* this means that the header will always be displayed */}
-      <Header></Header>
+      <Header currentUser={this.state.currentUser}></Header>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path='/shop' component={ShopPage} />
@@ -20,6 +46,7 @@ function App() {
       </Switch>
     </div>
   );
+  }
 }
 
 export default App;
